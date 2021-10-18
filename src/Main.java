f56070f381b6735908d60f12b9d7229145e1ac64
babs <babaly.diop91@gmail.com>
2021-10-18 08:47:55 +0200

    Ca marche

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
         // TODO Auto-generated method stub
 		
+		/*Test Pour babaly*/
 		System.out.println("Bienvenue sur l'application");
 }
 }
